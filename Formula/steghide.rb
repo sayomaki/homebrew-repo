9ddef3f6aac00d9b262a4b45a766b23f85e7aadb
willi123yao <willi123yao@gmail.com>
2022-06-23 00:04:28 +0800

    steghide 0.5.1 (new formula)
    Closes #1.
    
    Signed-off-by: sayonara <26355099+willi123yao@users.noreply.github.com>

---
+++ Formula/steghide.rb
@@ -0,0 +1,30 @@
+class Steghide < Formula
+  desc "Steganography program able to hide data in image and audio files"
+  homepage "https://steghide.sourceforge.io"
+  url "https://downloads.sourceforge.net/project/steghide/steghide/0.5.1/steghide-0.5.1.tar.gz"
+  sha256 "78069b7cfe9d1f5348ae43f918f06f91d783c2b3ff25af021e6a312cf541b47b"
+  license "GPL-1.0-or-later"
+
+  depends_on "libtool" => :build
+  depends_on "gettext"
+  depends_on "jpeg"
+  depends_on "mcrypt"
+  depends_on "mhash"
+
+  patch do
+    url "https://rawcdn.githack.com/willi123yao/brew-repo/a415d8e1ceb8a9b9ed5211f83ac354f4f47b0216/Patches/steghide-gcc.diff"
+    sha256 "75fd41eeecdf5446d5d693fefc651ba3429c70e3133fbb2c0b0d78615ccbdaf0"
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--prefix=#{prefix}", "--mandir=#{man}"
+    inreplace "src/Makefile", "SHELL = /bin/sh", "SHELL = /bin/bash" if OS.mac?
+    inreplace "src/Makefile", "LIBTOOL = $(SHELL) libtool", "LIBTOOL = $(SHELL) glibtool --tag CXX" if OS.mac?
+    inreplace "src/Makefile", "LIBTOOL = $(SHELL) libtool", "LIBTOOL = $(SHELL) libtool --tag CXX" if OS.linux?
+    system "make", "install"
+  end
+
+  test do
+    system bin/"steghide", "--version"
+  end
+end
