d9418af13e452fa2d4ec59bbdd655c0470c1397b
sayonara <26355099+willi123yao@users.noreply.github.com>
2022-07-06 16:02:06 +0000

    stegsnow: add 20130616 bottle.

--- Formula/stegsnow.rb
+++ Formula/stegsnow.rb
@@ -5,6 +5,12 @@ class Stegsnow < Formula
   sha256 "49179a21a2892297053db3d7ef41e00c83dbebea30c472487a6d3975b877ebc8"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/willi123yao/homebrew-repo/releases/download/stegsnow-20130616"
+    sha256 cellar: :any_skip_relocation, big_sur:      "776369cf99297d83a815ac7e6df767633f136592c97e94e578e326311dfe1aad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ac0ffc5edb497d3e4aa7146ac9a051921e01265b72c72599b34f77f46415fbd1"
+  end
+
   patch do
     url "https://aur.archlinux.org/cgit/aur.git/plain/stegsnow.patch?h=stegsnow"
     sha256 "d27d97a950a99f649b705d03ef334fa33bfb76280cb6fcf21a73ad4c0fdcaafc"
