85ea90d2285b5422cfff1490976b1f9848b5b7fb
sayonara <26355099+willi123yao@users.noreply.github.com>
2022-07-11 04:48:15 +0000

    dbus-test-runner: add 19.04.0 bottle.

--- Formula/dbus-test-runner.rb
+++ Formula/dbus-test-runner.rb
@@ -5,6 +5,12 @@ class DbusTestRunner < Formula
   sha256 "645a32fbd909baf2c01438f0cbda29dc9cd01a7aba5504c45610d88e8a57cb76"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/willi123yao/homebrew-repo/releases/download/dbus-test-runner-19.04.0"
+    sha256 big_sur:      "527fdb8d0ad3aaf6e02a83613465ee81fb2b4940c7ada1d4e325edf8232b7042"
+    sha256 x86_64_linux: "a82f7d2ed2c5352df20151f4dea62942bbb9caa888537634fe4577af17704d06"
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
   depends_on "dbus-glib"
