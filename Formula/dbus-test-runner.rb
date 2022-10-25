eb4d71ffd7c1efda38c12b5eb23a931e3446287e
sayonara <26355099+willi123yao@users.noreply.github.com>
2022-10-25 09:04:33 +0000

    dbus-test-runner: update 19.04.0 bottle.

--- Formula/dbus-test-runner.rb
+++ Formula/dbus-test-runner.rb
@@ -7,7 +7,9 @@ class DbusTestRunner < Formula
 
   bottle do
     root_url "https://github.com/willi123yao/homebrew-repo/releases/download/dbus-test-runner-19.04.0"
+    sha256 monterey:     "13642fc792b9f8b80d3d4fbec0d9cc921b3df3844e445cd4eb9fdd1a187d4c54"
     sha256 big_sur:      "527fdb8d0ad3aaf6e02a83613465ee81fb2b4940c7ada1d4e325edf8232b7042"
+    sha256 catalina:     "d4a7bce8cd1d2ad9315c8821cbc6c72d3f48840b40c1dded70b93c3e7b710b7d"
     sha256 x86_64_linux: "a82f7d2ed2c5352df20151f4dea62942bbb9caa888537634fe4577af17704d06"
   end
 
